cebb33791f0af44e20e13414cea529da012ce5a6
Эл <i.choknutuy.psix@gmail.com>
2025-04-16 13:18:38 +0300

    Add Lesson_6 on git

---
+++ src/main/java/Lesson_6/MyArraySizeException.java
@@ -0,0 +1,11 @@
+package Lesson_6;
+
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+
+    public MyArraySizeException() {
+
+    }
+}
